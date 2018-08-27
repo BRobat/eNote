1182e28b66616324155e5a1e261b13217c53b785
BRobat <Patogeen@icloud.com>
2018-08-27 21:42:49 +0200

    nearly finished styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
@@ -18,6 +19,7 @@ import { LoginPageComponent } from './login-page/login-page.component';
     LoginPageComponent
   ],
   imports: [
+    FormsModule,
     BrowserModule
   ],
   providers: [],
